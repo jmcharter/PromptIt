057be4ef73a8c3bea6a463db7cc1f6721abbd3b9
Jon Charter <jmcharter@gmail.com>
2022-01-30 18:43:38 +0000

    Add prompts controller

---
+++ server/src/controllers/prompts.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import Prompt from "../models/prompt";
+import User from "../models/user";
+
+
+export const getPrompts = async (req: Request, res: Response) => {
+    try {
+        const prompts = await Prompt.find();
+        console.log(prompts);
+        res.status(200).json(prompts);
+    } catch (error: any) {
+        res.status(404).json({ message: error.message });
+    }
+};
+
+export const createPrompt = async (req: Request, res: Response) => {
+    const body = req.body;
+    try {
+        const { title, text, id } = body;
+        const existingTitle = await Prompt.findOne({ title });
+        if (existingTitle) {
+            return res.status(400).json({ message: "This title has already been used. Please choose another one." });
+        }
+        const user: any = await User.findById(id);
+        console.log(user);
+        const newPrompt = new Prompt({
+            title: title,
+            text: text,
+            createdBy: user,
+        });
+        newPrompt.save();
+        user.prompts.push(newPrompt._id);
+        user.save();
+        console.log(user);
+        res.status(201).json({ message: "Prompt posted successfully", prompt: { title, text, id } });
+
+    } catch (error: any) {
+        res.status(404).json({ message: error.message });
+    }
+};
+
+export const getNonRemovedPrompts = async () => {
+
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import 'dotenv/config';
 
 import userRoutes from './routes/users';
+import promptRoutes from './routes/prompts';
 
 const app: Application = express();
 
@@ -13,6 +14,7 @@ app.use(express.json({ limit: "30mb" }));
 app.use(cors());
 
 app.use('/users', userRoutes);
+app.use('/prompts', promptRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     console.log("Hello, world");
