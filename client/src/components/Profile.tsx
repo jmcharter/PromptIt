c09172a235d1a855f4ce7709041ef73ae26fc0e9
Jon Charter <jmcharter@gmail.com>
2022-01-30 01:27:31 +0000

    Add redirect when not logged in

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,15 +1,25 @@
 import React, { ReactElement } from 'react';
 import { getUser } from '../api';
 import { useAuth } from '../contexts/AuthContext';
+import { Navigate } from 'react-router-dom';
 
 interface User {
     name: string;
 }
 
+const isLoggedIn = () => {
+    return !!localStorage.getItem("access-token");
+};
+
+
 function Profile(): ReactElement {
     const { currentUser } = useAuth();
+    // if (!isLoggedIn()) {
+    //     return <Navigate to="login" />;
+    // }
     return (
         <div>
+            {!isLoggedIn() && <Navigate to="login" />}
             <h1>Hello {currentUser.displayName}</h1>
         </div>
     );
