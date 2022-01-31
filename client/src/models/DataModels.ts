12737ed592053d9d2befa559c98da936e7adb976
Jon Charter <jmcharter@gmail.com>
2022-01-31 11:50:34 +0000

    Add PromptData interface

--- client/src/models/DataModels.ts
+++ client/src/models/DataModels.ts
@@ -2,5 +2,12 @@ export interface UserData {
     _id?: string;
     displayName: string;
     username: string;
-    email: string;
+    email?: string;
+}
+
+export interface PromptData {
+    _id: string;
+    title: string;
+    text: string;
+    createdBy: UserData;
 }
