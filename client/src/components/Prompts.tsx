821ae2db531c472a0e5f82ab42c8783739e9c877
Jon Charter <jmcharter@gmail.com>
2022-01-31 11:50:21 +0000

    Add layout for prompts component

--- client/src/components/Prompts.tsx
+++ client/src/components/Prompts.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
+import { Container, Grid } from '@mui/material';
+import { Box } from '@mui/system';
+import React, { useEffect, useState } from 'react';
+import { getPrompts } from '../api';
+import { PromptData } from '../models/DataModels';
+import Prompt from './Prompt';
 
 interface Props {
 
 }
 
 const Prompts = (props: Props) => {
-    return (
-        <div>
+    const [prompts, setPrompts] = useState<[PromptData]>([{ _id: "", title: "", text: "", createdBy: { displayName: "", username: "" } }]);
+    const [loading, setLoading] = useState<Boolean>(true);
+    const fetchData = async () => {
+        const res = await getPrompts();
+        const data = res.data;
+        setPrompts(data);
+        console.log(data);
+        setLoading(false);
+    };
 
-        </div>
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            {!loading &&
+                <Container
+                    sx={{ mt: 2 }}
+                >
+                    <Box
+                        sx={{ flexGrow: 1 }}
+                    >
+                        <Grid container spacing={1}>
+                            {prompts && prompts.map(prompt =>
+                            (
+                                <Grid item xs={12} md={6}>
+                                    <Prompt
+                                        key={prompt._id}
+                                        _id={prompt._id}
+                                        title={prompt.title}
+                                        text={prompt.text}
+                                        user={prompt.createdBy.displayName}
+                                    />
+                                </Grid>
+                            ))
+                            }
+                        </Grid>
+
+                    </Box>
+                </Container>
+            }
+        </>
     );
 };
 
